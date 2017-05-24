9c3ca62b3fd6a146f92e577498b4c7b600ead4e3
Administrator <admin@example.com>
2017-05-24 09:41:35 +0800

    start servlet

---
+++ src/main/java/web/TimeServlet.java
@@ -0,0 +1,21 @@
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dongrui on 2017/5/23.
+ */
+public class TimeServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.使用request接收请求数据
+        //1.1请求行
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
