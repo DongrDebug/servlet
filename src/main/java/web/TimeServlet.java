9bd35ae2c3adf2ea596e40ce7d9f9f69596d5592
Administrator <admin@example.com>
2017-05-24 11:43:58 +0800

    start servlet

--- src/main/java/web/TimeServlet.java
+++ src/main/java/web/TimeServlet.java
@@ -5,6 +5,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 /**
  * Created by dongrui on 2017/5/23.
@@ -14,10 +19,27 @@ public class TimeServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1.使用request接收请求数据
         //1.1请求行
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("ajskdalsdlka");
+        System.out.println("协议类型" + req.getPathInfo());
+        System.out.println("访问路径" + req.getServletPath());
+        System.out.println("请求方式" + req.getMethod());
+        //1.2消息头(key-value)
+        //Enumeration是比Iterator更早的迭代器
+        Enumeration<String> e = req.getHeaderNames();
+        while (e.hasMoreElements()) {
+            String key = e.nextElement();
+            String value = req.getHeader(key);
+            System.out.println(key + ":" + value);
+        }
+        //1.3实体内容
+        //本案例没传具体内容，所以实体内容为空
+        //2.使用response发送响应数据
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String now = sdf.format(date);
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        //此处偷懒输出一个简化后的页面
+        out.println("<p>" + now + "</p>");
+        out.close();
     }
 }
